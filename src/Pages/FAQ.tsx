9ab2890e5ed55a36ff3fedaf9a75e812a8abd73a
mkiki3 <kiersten.magee@morsco.com>
2023-11-29 10:45:43 -0600

    update custom hook and component

--- src/Pages/FAQ.tsx
+++ src/Pages/FAQ.tsx
@@ -1,22 +1,29 @@
-import React from 'react';
-// import { users } from '../data/index';
+import React, { useState, useCallback } from 'react';
+import { users } from '../data/index';
 import classNames from 'classnames';
 import useInfiniteScroll from '../hooks/useInfinitScroll';
 
 const FAQ: React.FC = () => {
-  //   const container = myElementRef.current;
-  //   if (container) {
-  //     const { scrollHeight, scrollTop, clientHeight } = container;
-  //     if (scrollHeight - scrollTop === clientHeight) {
-  //       // User has scrolled to the bottom, fetch more users
-  //       fetchMoreUsers();
-  //     }
-  //   }
-  //   updateScrollHeight(); // Update scroll information
-  // };
+  const [visibleUsers, setVisibleUsers] = useState(users.slice(0, 4));
+  const [loading, setLoading] = useState(false);
+  const pageSize = 3;
 
-  const { myElementRef, scrollInfo, visibleUsers, loading, handleScroll } =
-    useInfiniteScroll(3);
+  const fetchMoreUsers = useCallback(async () => {
+    setLoading(true);
+    // Simulating fetching more users with a delay (you can replace this with an actual API call)
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    // Simulating fetching more users
+    const newUsers = users.slice(
+      visibleUsers.length,
+      visibleUsers.length + pageSize
+    );
+    console.log(newUsers);
+    setVisibleUsers((prevUsers) => [...prevUsers, ...newUsers]);
+    setLoading(false);
+  }, [visibleUsers]);
+
+  const { myElementRef, scrollInfo, handleScroll } =
+    useInfiniteScroll(fetchMoreUsers);
 
   return (
     <div className=''>
@@ -47,30 +54,3 @@ const FAQ: React.FC = () => {
 };
 
 export default FAQ;
-
-/**
- *  <div className=''>
-      <div className='container-sizes'>
-        <p>
-          Scroll Height: {scrollInfo.scrollHeight} ScrollTop:{' '}
-          {scrollInfo.scrollTop} ClientHeight: {scrollInfo.clientHeight}
-        </p>
-      </div>
-      <div className='container'>
-        <div
-          onScroll={handleScroll}
-          className={classNames(
-            'my-12 border bg-blue-200 flex-col justify-center items-center overflow-y-auto max-h-36'
-          )}
-          ref={myElementRef}
-        >
-          {visibleUsers.map((x: string, index: number) => (
-            <div key={index} className={classNames('p-2 border')}>
-              {x}
-            </div>
-          ))}
-          {loading && <div>Loading...</div>}
-        </div>
-      </div>
-    </div>
- */
--- src/hooks/useInfinitScroll.tsx
+++ src/hooks/useInfinitScroll.tsx
@@ -1,25 +1,19 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import { useRef, useState, useCallback, useEffect, useMemo } from 'react';
-import { users } from '../data/index';
+import { useRef, useState } from 'react';
 
-const useInfiniteScroll = (pageSize: number) => {
+const useInfiniteScroll = (callback: () => void) => {
   const myElementRef = useRef<HTMLDivElement>(null);
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+
   const [scrollInfo, setScrollInfo] = useState({
     scrollHeight: 0,
     scrollTop: 0,
     clientHeight: 0,
   });
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const [loading, setLoading] = useState(false);
   /**
    * The slice function is used to create a shallow copy of a portion of the
    *  users array. In this case, it's creating a copy of the first 4 items
    * in the users array, and this is the initial set of visible users.
    */
-  const [visibleUsers, setVisibleUsers] = useState(users.slice(0, 4)); // Initial visible users
-  //   const pageSize = 3; // Number of items to show per page
 
   const updateScrollHeight = () => {
     if (myElementRef.current) {
@@ -28,33 +22,19 @@ const useInfiniteScroll = (pageSize: number) => {
     }
   };
 
-  const fetchMoreUsers = useCallback(async () => {
-    setLoading(true);
-    // Simulating fetching more users with a delay (you can replace this with an actual API call)
-    await new Promise((resolve) => setTimeout(resolve, 1000));
-    // Simulating fetching more users
-    const newUsers = users.slice(
-      visibleUsers.length,
-      visibleUsers.length + pageSize
-    );
-    console.log(newUsers);
-    setVisibleUsers((prevUsers) => [...prevUsers, ...newUsers]);
-    setLoading(false);
-  }, [visibleUsers]);
-
   const handleScroll = () => {
     const container = myElementRef.current;
     if (container) {
       const { scrollHeight, scrollTop, clientHeight } = container;
       if (scrollHeight - scrollTop === clientHeight) {
         // User has scrolled to the bottom, fetch more users
-        fetchMoreUsers();
+        callback();
       }
     }
     updateScrollHeight(); // Update scroll information
   };
 
-  return { myElementRef, scrollInfo, visibleUsers, loading, handleScroll };
+  return { myElementRef, scrollInfo, handleScroll };
 };
 
 export default useInfiniteScroll;
