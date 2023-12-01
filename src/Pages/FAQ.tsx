f02145366390b668964fe515ef8dfb199e73ad5d
mkiki3 <kiersten.magee@morsco.com>
2023-12-01 14:21:55 -0600

    add navigation for FAQ

--- src/Pages/FAQ.tsx
+++ src/Pages/FAQ.tsx
@@ -1,10 +1,29 @@
 import React from 'react';
-import Scroll from '../Component/Scroll';
+import { Link } from 'react-router-dom';
 
 const FAQ: React.FC = () => {
   return (
-    <div className=''>
-      <Scroll />
+    <div className=' flex flex-col  items-center'>
+      <div className='text-5xl pt-20 pb-16'>
+        Frequently <span className='text-pink-250'>Ask Questions</span>
+      </div>
+      <div className='flex justify-between w-1/4'>
+        <span className='hover:border hover:border-white hover:border-b-pink-500 pb-1'>
+          <Link to='/faq'>General</Link>
+        </span>
+        <span className='hover:border hover:border-white hover:border-b-pink-500 pb-1'>
+          <Link to='/faq'>Service</Link>
+        </span>
+        <span className='hover:border hover:border-white hover:border-b-pink-500 pb-1'>
+          <Link to='/faq'>Payment</Link>
+        </span>
+        <span className='hover:border hover:border-white hover:border-b-pink-500 pb-1'>
+          <Link to='/faq'>Refund</Link>
+        </span>
+        <span className='hover:border hover:border-white hover:border-b-pink-500 pb-1'>
+          <Link to='/faq'>Return</Link>
+        </span>
+      </div>
     </div>
   );
 };
