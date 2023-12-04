aa95caf276c88296213f235db1d724bf0f615af1
mkiki3 <kiersten.magee@morsco.com>
2023-12-04 12:33:28 -0600

    add comments and accordion

--- src/data/index.ts
+++ src/data/index.ts
@@ -21,3 +21,94 @@ export const users = [
   'Kim',
   'Chyna',
 ];
+
+export const faq = [
+  {
+    question: 'What is your refund policy?',
+    answer:
+      ' If youre unhappy with your purchase for any reason, email us within 90 days and well refund you in full, no questions asked.',
+  },
+  {
+    question: 'Do you offer custom cake orders for special occasions?',
+    answer: 'yes',
+  },
+  {
+    question: 'Do you accommodate dietary restrictions or allergies?',
+    answer: 'yes',
+  },
+  {
+    question:
+      'How far in advance should I place an order for a custom cake?    ',
+    answer:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sapien ligula, sagittis non neque in.',
+  },
+  {
+    question: 'What are your payment methods',
+    answer:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sapien ligula, sagittis non neque in.',
+  },
+  {
+    question: 'What is your policy on returns or refunds?',
+    answer:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sapien ligula, sagittis non neque in.',
+  },
+  {
+    question: 'Do you offer catering services for events?',
+    answer:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sapien ligula, sagittis non neque in.',
+  },
+  {
+    question:
+      'Can I schedule a tasting for wedding cakes or other large orders?',
+    answer:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sapien ligula, sagittis non neque in.',
+  },
+  {
+    question: 'What is your cancellation policy for custom orders?',
+    answer:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sapien ligula, sagittis non neque in.',
+  },
+  {
+    question: 'Do you offer seasonal or holiday-specific treats?',
+    answer:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sapien ligula, sagittis non neque in.',
+  },
+  {
+    question: 'Under what circumstances can I request a refund?',
+    answer:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sapien ligula, sagittis non neque in.',
+  },
+  {
+    question: 'How do I initiate a refund for a purchase?',
+    answer:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sapien ligula, sagittis non neque in.',
+  },
+  {
+    question: 'Is there a time limit for requesting a refund on bakery items?',
+    answer:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sapien ligula, sagittis non neque in.',
+  },
+  {
+    question:
+      'Can I get a refund if I am not satisfied with the taste or quality of the product?',
+    answer:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sapien ligula, sagittis non neque in.',
+  },
+  {
+    question: 'What is your policy on refunds for canceled orders?',
+    answer:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sapien ligula, sagittis non neque in.',
+  },
+  {
+    question:
+      'Are there specific conditions that must be met for a refund to be approved?',
+    answer:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sapien ligula, sagittis non neque in.',
+  },
+  {
+    question:
+      'Is there a customer service contact for questions or concerns regarding refunds and returns?',
+    answer:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sapien ligula, sagittis non neque in.',
+  },
+];
---
+++ src/types/faq.ts
@@ -0,0 +1,4 @@
+export interface faqProp {
+  question: string;
+  answer: string;
+}
