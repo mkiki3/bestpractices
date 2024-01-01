b3dc85c80bb2f1767901a5b50277d6881be073c1
mkiki3 <kiersten.magee@morsco.com>
2024-01-01 00:06:39 -0600

    add redux toolkit

--- src/Component/QtyPicker/index.tsx
+++ src/Component/QtyPicker/index.tsx
@@ -1,32 +1,38 @@
-import React, { useState } from 'react';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  setQuantity,
+  incrementQuantity,
+  decrementQuantity,
+} from '../../store/slices/quantitySlice';
+import { RootState } from '../../store';
 // import Button from '../Button';
 
 const QtyPicker: React.FC = () => {
-  const [quantity, setQuantity] = useState(0);
+  // const [quantity, setQuantity] = useState(0);
+  const quantity = useSelector((state: RootState) => state.quantity.value);
+  const dispatch = useDispatch();
 
   return (
     <div className='flex'>
-      <button onClick={() => updateQty('decrement')} className='border px-3'>
+      <button
+        onClick={() => dispatch(decrementQuantity())}
+        className='border px-3'
+      >
         -
       </button>
       <div className='border px-3 border-l-white border-r-white'>
         {quantity}
       </div>
-      <button onClick={() => updateQty('increment')} className='border px-3'>
+      <button
+        onClick={() => dispatch(incrementQuantity())}
+        className='border px-3'
+      >
         +
       </button>
     </div>
   );
-
-  function updateQty(action: string) {
-    setQuantity((prev: number) =>
-      action === 'increment'
-        ? prev + 1
-        : action === 'decrement' && prev !== 0
-        ? prev - 1
-        : prev
-    );
-  }
 };
 
 export default QtyPicker;
